09298a83750531f42159a81eb71e2e6de448a5a4
Denis Kipchakbaev <denis.kipchakbaev@weekendesk.fr>
2020-04-16 18:38:16 +0200

    GREEN: refactor tests for single number

--- src/test/java/com/codurance/meetup/CalculatorShould.java
+++ src/test/java/com/codurance/meetup/CalculatorShould.java
@@ -14,16 +14,10 @@ public class CalculatorShould {
     }
 
     @Test
-    public void return_four_when_four_string() {
-
-        Calculator calculator = new Calculator();
-        assertEquals(4, calculator.add("4"));
-    }
-
-    @Test
     public void return_number_when_pass_a_number() {
-
         Calculator calculator = new Calculator();
         assertEquals(6, calculator.add("6"));
+        assertEquals(4, calculator.add("4"));
+        assertEquals(100, calculator.add("100"));
     }
 }
