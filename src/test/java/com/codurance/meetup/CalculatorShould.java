60a7a32c62c0558bb265348ef8a17abcfa0a2c4d
Alejandro Martínez <alex1983bcn@gmail.com>
2020-04-16 19:14:55 +0200

    GREEN: refactor of CalculatorShould tests

--- src/test/java/com/codurance/meetup/CalculatorShould.java
+++ src/test/java/com/codurance/meetup/CalculatorShould.java
@@ -19,14 +19,10 @@ public class CalculatorShould {
     }
 
     @Test
-    public void return_sum_when_pass_two_numbers() {
+    public void return_the_sum_of_given_numbers() {
         assertEquals(3, Calculator.add("1,2"));
         assertEquals(6, Calculator.add("3,3"));
         assertEquals(20, Calculator.add("10,10"));
-    }
-
-    @Test
-    public void return_the_sum_of_given_numbers() {
         assertEquals(45, Calculator.add("1,2,3,4,5,6,7,8,9"));
     }
 }
