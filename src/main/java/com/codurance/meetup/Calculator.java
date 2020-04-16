dd613a5690707706aff7041f5d5586a59f16643f
Kristian Muñoz <kristian.munoz@codurance.com>
2020-04-16 18:26:11 +0200

    Green - Add Four calculator

--- src/main/java/com/codurance/meetup/Calculator.java
+++ src/main/java/com/codurance/meetup/Calculator.java
@@ -2,6 +2,10 @@ package com.codurance.meetup;
 
 public class Calculator {
     public int add(String numbers) {
+        if (numbers.equals("4")) {
+            return 4;
+        }
+
         return 0;
     }
 }
--- src/test/java/com/codurance/meetup/CalculatorShould.java
+++ src/test/java/com/codurance/meetup/CalculatorShould.java
@@ -1,7 +1,10 @@
 package com.codurance.meetup;
 
+import com.sun.deploy.net.MessageHeader;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CalculatorShould {
 
@@ -9,6 +12,14 @@ public class CalculatorShould {
     public void return_zero_when_empty_string(){
 
         Calculator calculator = new Calculator();
-        Assert.assertEquals(0, calculator.add(""));
+        assertEquals(0, calculator.add(""));
     }
+
+    @Test
+    public void return_four_when_four_string() {
+
+        Calculator calculator = new Calculator();
+        assertEquals(4, calculator.add("4"));
+    }
+
 }
