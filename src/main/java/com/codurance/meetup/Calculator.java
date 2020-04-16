df20ae167aee3f608b07c4f0655505a4743d42b4
Denis Kipchakbaev <denis.kipchakbaev@weekendesk.fr>
2020-04-16 19:18:33 +0200

    GREEN: refactor add method

--- src/main/java/com/codurance/meetup/Calculator.java
+++ src/main/java/com/codurance/meetup/Calculator.java
@@ -11,13 +11,10 @@ public class Calculator {
     public static int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
-        } else if (numbers.contains(",")){
-            String[] arrayNumbers = numbers.split(",");
-            return Arrays.stream(arrayNumbers)
-                    .mapToInt(Integer::parseInt)
-                    .sum();
         }
-
-        return Integer.parseInt(numbers);
+        String[] arrayNumbers = numbers.split(",");
+        return Arrays.stream(arrayNumbers)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
